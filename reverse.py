84c9f10f03c9bca3b3a3af96ef7869720a348cc8
sHaViNX <88244172+sHaViNX@users.noreply.github.com>
2023-10-31 22:18:16 +0530

    Create reverse.py
    reverse a string

---
+++ reverse.py
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char* str) {
+    int length = strlen(str);
+    for (int i = 0; i < length / 2; i++) {
+        char temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
+    }
+}
+
+int main() {
+    char inputString[100];
+    
+    // Read the input string
+    scanf("%s", inputString);
+    
+    // Reverse the string
+    reverseString(inputString);
+    
+    // Print the reversed string
+    printf("%s\n", inputString);
+    
+    return 0;
+}
